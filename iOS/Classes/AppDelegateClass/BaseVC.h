ea6bad8669f4355b2d049664e29225d095fbbc56
AdamBahr <adam.bahr.333@gmail.com>
2015-09-04 09:44:10 -0400

    60hrs
    final commit for week

--- iOS/Classes/AppDelegateClass/BaseVC.h
+++ iOS/Classes/AppDelegateClass/BaseVC.h
@@ -1,11 +1,3 @@
-//
-//  BaseVC.h
-//  Tinder
-//
-//  Created by Elluminati - macbook on 12/04/14.
-//  Copyright (c) 2014 AppDupe. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <MessageUI/MessageUI.h>
 #import <MessageUI/MFMailComposeViewController.h>
